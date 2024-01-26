ac6faefc722b6465a88f2051addc05d89181d2ef
RaihaniSue <sue.raih@gmail.com>
2024-01-26 11:58:08 -0800

    Zombie processes to be created

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,46 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+ * infinite_while - entry point
+ *
+ * Return: 0 always on success
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - entry point
+ *
+ * Return: 0 always on success
+ */
+int main(void)
+{
+	pid_t pid;
+	char c = 0;
+
+	while (c < 5)
+	{
+		pid = fork();
+		if (pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", pid);
+			sleep(1);
+			c++;
+		}
+		else
+			exit(0);
+	}
+
+	infinite_while();
+	return(EXIT_SUCCESS);
+}
